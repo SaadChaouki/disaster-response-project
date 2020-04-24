75346008fdc4b740b24f44993c54d79aea14e528
SaadChaouki <61946524+SaadChaouki@users.noreply.github.com>
2020-04-24 16:19:59 +0100

    Files Changes

---
+++ app/run.py
@@ -0,0 +1,200 @@
+import json
+import plotly
+import pandas as pd
+import nltk
+import re
+import sys
+
+import nltk
+from nltk import pos_tag
+from nltk.corpus import wordnet, stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+
+# Download nltk ressources
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
+
+
+
+def wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+def tokenize(text):
+    contract_dict = {"ain't": "am not", "aren't": "am not", "can't": "cannot", "can't've": "cannot have",
+    "'cause": "because", "could've": "could have", "couldn't": "could not", "couldn't've": "could not have",
+    "didn't": "did not", "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hadn't've": "had not have",
+    "hasn't": "has not", "haven't": "have not", "he'd": "he would",  "he'd've": "he would have", "he'll": "he will",
+    "he'll've": "he will have", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will",
+    "how's": "how is", "i'd": "I would", "i'd've": "I would have", "i'll": "I will", "i'll've": "I will have",
+    "i'm": "i am", "i've": "I have", "isn't": "is not", "it'd": "it would","it'd've": "it would have",
+    "it'll": "t will", "it'll've": "it will have", "it's": "t is", "let's": "let us", "ma'am": "madam",
+    "mayn't": "may not", "might've": "might have", "mightn't": "might not", "mightn't've": "might not have",
+    "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not",
+    "needn't've": "need not have", "o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have",
+    "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would",
+    "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is",
+    "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have",
+    "so's": "so is", "that'd": "that would", "that'd've": "that would have", "that's": "that is",
+    "there'd": "there would", "there'd've": "there would have", "there's": "there is", "they'd": "they would",
+    "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are",
+    "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have",
+    "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not",
+    "what'll": "what will", "what'll've": "what will have", "what're": "what are", "what's": "what is",
+    "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is",
+    "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have",
+    "why's": "why is", "why've": "why have","will've": "will have", "won't": "will not","won't've": "will not have",
+    "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all",
+    "y'all'd": "you all would", "y'all'd've": "you all would have", "y'all're": "you all are", "y'all've": "you all have",
+    "you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have",
+    "you're": "you are", "you've": "you have"}
+
+    # Finding and replacing urls
+    text = re.sub('(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?',  'placeholder', text)
+
+    # Replace contractions
+    for key in contract_dict:
+        text = text.replace(key.lower(), contract_dict[key])
+
+    # Remove special characters
+    text = re.sub('[^0-9a-zA-Z]+', ' ', text)
+
+    # Lower
+    text = text.lower()
+
+    # stop words
+    stop_words = stopwords.words("english")
+
+    # Tokens
+    tokens = word_tokenize(text)
+
+    # Part of speech
+    pos_tokens = pos_tag(tokens)
+
+    # Lemmatize
+    clean_tokens = [WordNetLemmatizer().lemmatize(token[0], wordnet_pos(token[1])) for token in pos_tokens if token[0] not in stop_words]
+
+    return clean_tokens
+
+
+app = Flask(__name__)
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('emergency_messages', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+# index webpage displays cool visuals and receives user input text for model
+@app.route('/')
+@app.route('/index')
+def index():
+
+    # extract data needed for visuals
+    genre_counts = df.groupby('genre').count()['message']
+    genre_names = list(genre_counts.index)
+    cat_counts = df.drop(['message', 'genre'], axis=1).sum()
+
+    # create visuals
+    graphs = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message Genres',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=cat_counts.index,
+                    y=cat_counts.values
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+
+        }
+    ]
+
+    # encode plotly graphs in JSON
+    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # render web page with plotly graphs
+    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '')
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(zip(df.columns[4:], classification_labels))
+
+    # This will render the go.html Please see that file.
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
